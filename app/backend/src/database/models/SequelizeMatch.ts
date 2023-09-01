1fc114da57f8f0522d98c626779a85cd7e5a626b
Gabriella Vieira <gabvi2015@outlook.com>
2023-09-01 11:23:35 -0300

    feat: req 13 - matches migration and model

---
+++ app/backend/src/database/models/SequelizeMatch.ts
@@ -0,0 +1,66 @@
+import { DataTypes, InferAttributes, InferCreationAttributes, Model } from 'sequelize';
+import db from '.';
+import SequelizeTeam from './SequelizeTeam';
+
+class SequelizeMatch extends Model<InferAttributes<SequelizeMatch>,
+InferCreationAttributes<SequelizeMatch>> {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+SequelizeMatch.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+SequelizeTeam.hasMany(SequelizeMatch, { foreignKey: 'homeTeamId' });
+SequelizeMatch.belongsTo(SequelizeTeam, { foreignKey: 'homeTeamId' });
+
+SequelizeTeam.hasMany(SequelizeMatch, { foreignKey: 'awayTeamId' });
+SequelizeMatch.belongsTo(SequelizeTeam, { foreignKey: 'awayTeamId' });
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default SequelizeMatch;
